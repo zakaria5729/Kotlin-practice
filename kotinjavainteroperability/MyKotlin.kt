f6c33146653a1f0515d9eba6e9758dac6c04e847
zakaria5729 <zakariahossain143@gmail.com>
2019-02-06 17:06:34 +0600

    Calling Kotlin functions from Java file and vice-versa and kotlin function

--- kotinjavainteroperability/MyKotlin.kt
+++ kotinjavainteroperability/MyKotlin.kt
@@ -4,4 +4,4 @@ fun main(args: Array<String>) {
     println("Printing sum from Kotlin file: ${MyJava.sumFromJava(2,5)}")
 }
 
-fun sumFromKotlin(x: Int, y: Int): Int = x+y
+fun sumFromKotlin(x: Int, y: Int): Int = x + y
